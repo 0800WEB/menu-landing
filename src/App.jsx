0cb018bc771cc57f0e570ccf90bdb790f07a4c0e
Paolo Suarez <ptsuarez91@hotmail.com>
2024-09-10 14:05:10 -0400

    chore(setup): configurcion de proyecto con React, Vite y Tailwind CSS
    - Instalación de React y Vite
    - Configuración inicial de la estructura de carpetas
    - Instalación y configuración de Tailwind CSS
    - Definición de colores personalizados, sombras y radios en Tailwind
    - Adición de iconos para el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h4 className='font-bold text-4xl text-white bg-red-400'>hola mundo </h4>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        baseBg: "#FDF2E4",
+        blackPrimary: "#212121",
+        highlight: "#FF7300",
+        whiteCustom: "#ffffff",
+        colorIcon: "#212121",
+      },
+      borderRadius: {
+        radiusButton: "50px",
+      },
+      boxShadow: {
+        shadowButtons: "0 4px 0 4px rgba(0, 0, 0, 0.25)",
+        shadowFood: "5px 20px 20px 0 rgba(0, 0, 0, 0.4)",
+      },
+    },
+  },
+  plugins: [],
+};
